8642dcfff702909060c2127992a558e2f5ef1b5b
Dean Lark <deanolark@gmail.com>
2025-01-27 21:00:02 +0000

    feat: added routes and templates for about, login, and register pages

--- app/routes.py
+++ app/routes.py
@@ -11,3 +11,23 @@ main = Blueprint("main", __name__)
 @main.route("/")
 def home():
     return render_template("index.html")
+
+
+@main.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@main.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@main.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@main.route("/logout")
+def logout():
+    return "Logic to logout"
