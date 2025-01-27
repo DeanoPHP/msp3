60685716014471ce08b2c1b1191ef6c82ace80ce
Dean Lark <deanolark@gmail.com>
2025-01-27 19:44:14 +0000

    feat: connected Flask app to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from dotenv import load_dotenv
 from .routes import main
+from .extensions import mongo, bcrypt
 
 
 def create_app():
     app = Flask(__name__)
 
     load_dotenv()
+
+    app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+    app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+    mongo.init_app(app)
+    bcrypt.init_app(app)
 
     app.register_blueprint(main)
 
