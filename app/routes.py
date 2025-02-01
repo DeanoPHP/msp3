23a8df72d5493d5ce805b0afbb23964d1b7076b2
Dean Lark <deanolark@gmail.com>
2025-02-01 18:05:51 +0000

    feat: add business form; chore: added comments to improve code readability

--- app/routes.py
+++ app/routes.py
@@ -17,8 +17,13 @@ from flask_pymongo import ObjectId
 main = Blueprint("main", __name__)
 
 
-# function to get the current user
 def get_current_user():
+    """
+    Gets the current logged in user
+
+    return:
+        dictionary: current users info
+    """
     if "user" not in session:
         return None
     return mongo.db.users.find_one({
@@ -28,6 +33,15 @@ def get_current_user():
 
 # function to get the profile user or current user
 def get_profile_user(username):
+    """
+    Gets the profile page user
+
+    Args:
+        username (str): username of the views profile page
+
+    return:
+        dictionary: profile owners information
+    """
     return mongo.db.users.find_one({
         "username": username
     })
@@ -35,16 +49,30 @@ def get_profile_user(username):
 
 @main.route("/")
 def home():
+    """Renders the home page"""
     return render_template("index.html")
 
 
 @main.route("/about")
 def about():
+    """Renders the about page"""
     return render_template("about.html")
 
 
 def getImages():
-    # Handle image upload
+    """
+    Handles image upload, encodes it in Base64, and returns the encoded data.
+
+    This function checks if a file is uploaded under the "profile_image" key in the request.
+    If a valid image file is found, it reads the file and encodes it in Base64 format.
+
+    Returns:
+        str or None: The Base64-encoded image data if successful, otherwise None.
+
+    Raises:
+        Exception: If an error occurs while reading the image, it logs the error,
+                   flashes a failure message, and redirects to the registration page.
+    """
     image_data = None
     if "profile_image" in request.files:
         image_file = request.files["profile_image"]
@@ -62,6 +90,22 @@ def getImages():
 
 @main.route("/register", methods=["GET", "POST"])
 def register():
+    """
+    Handles user registration.
+
+    If the request method is POST, the function:
+    - Checks if the username already exists in the database.
+    - If the username exists, flashes an error message and redirects back to the form.
+    - If not, processes the profile image (if provided).
+    - Hashes the password and stores user details in the database.
+    - Creates a session for the newly registered user.
+    - Redirects to the login page with a success message.
+
+    Returns:
+        - If the method is GET: Renders the registration form.
+        - If registration is successful: Redirects to the login page.
+        - If the username already exists: Redirects to the registration form with an error message.
+    """
     if request.method == "POST":
         # Check the user doesn't already exist
         existing_user = mongo.db.users.find_one({
@@ -100,6 +144,18 @@ def register():
 
 @main.route("/login", methods=["GET", "POST"])
 def login():
+    """
+    Handles user login.
+
+    If the request method is POST, the function:
+    - Retrieves the user from the database using the provided email.
+    - If the user exists, it verifies the password.
+    - If the password is correct, creates a session and redirects the user to their profile page.
+
+    Returns:
+        - If the method is GET: Renders the login form.
+        - If login is successful: Redirects to the user's profile page.
+    """
     if request.method == "POST":
         check_user = mongo.db.users.find_one({
             "email": request.form.get("email").lower()
@@ -118,11 +174,24 @@ def login():
 
             return redirect(url_for("main.profile", username=session["user"]))
 
+        flash("Sorry we cannot find that user", "danger")
+        return redirect(request.url)
+    
     return render_template("login.html")
 
 
 @main.route("/logout")
 def logout():
+    """
+    Handles user logout.
+
+    - Removes the "user" session variable.
+    - Flashes a success message to confirm logout.
+    - Redirects the user to the homepage.
+
+    Returns:
+        Redirects to the homepage after logging out.
+    """
     flash("You have been logged out", "success")
     session.pop("user")
     return redirect(url_for("main.home"))
@@ -130,8 +199,20 @@ def logout():
 
 @main.route("/profile/<username>")
 def profile(username):
+    """
+    Displays the profile page for a given username.
 
-    # find the profiles user
+    - Retrieves the user's profile data from the database.
+    - If the user is not found, flashes an error message and redirects to the homepage.
+    - If the user exists, renders the profile page with the user's data.
+
+    Args:
+        username (str): The username of the profile to display.
+
+    Returns:
+        - Renders the "profile.html" template with the user's data if found.
+        - Redirects to the homepage with an error message if no user is found.
+    """
     profile_user = get_profile_user(username)
 
     if not profile_user:
@@ -147,6 +228,25 @@ def profile(username):
 
 @main.route("/edit_details/<user_id>", methods=["GET", "POST"])
 def edit_details(user_id):
+    """
+    Handles user profile updates.
+
+    - Retrieves the current user's details from the database.
+    - If the user does not exist, flashes an error message and redirects to the profile page.
+    - Processes an optional profile image update.
+    - Updates only the fields provided in the form, keeping existing values for empty fields.
+    - Performs an update operation in the database.
+    - If the update is successful, flashes a success message and redirects to the profile page.
+    - If the update fails, flashes an error message.
+
+    Args:
+        user_id (str): The unique identifier of the user to update.
+
+    Returns:
+        - If the user is not found: Redirects to the profile page with an error message.
+        - If the update is successful: Redirects to the profile page with a success message.
+        - If an error occurs: Redirects to the profile page with an error message.
+    """
     if request.method == "POST":
         # Fetch the current user details from the database
         current_user = get_current_user()
@@ -187,3 +287,75 @@ def edit_details(user_id):
         except Exception as e:
             flash(f"An error occurred: {str(e)}", "danger")
             return redirect(url_for("main.profile", username=session['user']))
+
+
+@main.route("/add_business/<user_id>", methods=["GET", "POST"])
+def add_business(user_id):
+    """
+    Handles the addition of a new business by a logged-in user.
+
+    - Verifies that the user is logged in.
+    - Checks if images are uploaded; if not, flashes a warning message.
+    - Processes uploaded images by encoding them in Base64.
+    - Collects business details from the form and constructs a business object.
+    - Inserts the new business into the database.
+    - Redirects the user back to their profile with a success or error message.
+
+    Args:
+        user_id (str): The unique identifier of the user adding the business.
+
+    Returns:
+        - If the user is not logged in: Redirects to the profile page with an error message.
+        - If no images are uploaded: Redirects back to the form with a warning message.
+        - If the business is successfully added: Redirects to the user's profile with a success message.
+        - If an error occurs during insertion: Redirects to the profile page with an error message.
+    """
+    if request.method == "POST":
+
+        current_user = get_current_user()
+
+        if not current_user:
+            flash("You must be a logged in user to create a business", "danger")
+            return redirect(url_for("main.profile", username=current_user))
+
+        if "business_images" not in request.files or not request.files.getlist("business_images"):
+            flash("You have not uploaded any images", "warning")
+            return redirect(request.url)
+
+        # Get the files
+        files = request.files.getlist('business_images')
+        encoded_images = []
+
+        # Process each image
+        for file in files:
+            if file:  # Ensure the file is not empty
+                # Read image content
+                image_content = file.read()
+                # Encode image to Base64
+                encoded_image = base64.b64encode(
+                    image_content).decode("utf-8")
+                encoded_images.append(encoded_image)
+
+        business_to_add = {
+            "owner_id": ObjectId(user_id),
+            "company_name": request.form.get("company_name"),
+            "description": request.form.get("description"),
+            "location": request.form.get("location"),
+            "category": request.form.get("category"),
+            "images": encoded_images,
+            "contact_info": {
+                "email": request.form.get("email"),
+                "phone": request.form.get("phone"),
+                "website": request.form.get("website")
+            }
+        }
+
+        create_business = mongo.db.business.insert_one(business_to_add)
+
+        if not create_business:
+            flash("Something has gone wrong", "danger")
+            return redirect(url_for("main.profile", username=current_user['username']))
+
+        flash("Your business has been added successfully", "success")
+        print(current_user)
+        return redirect(url_for("main.profile", username=current_user['username']))
